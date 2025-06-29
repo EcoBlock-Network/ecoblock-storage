18791bb314fe67ff732d3a6ec21af0b44a2c5c4e
htande <htande@hotmail.fr>
2025-06-29 16:18:30 +0200

    feat(storage): setup public Tangle interface in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Ecoblock Storage – Local Tangle DAG construction and access
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod tangle;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use tangle::{Tangle, TangleError};
